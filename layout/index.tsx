b4843d83cbaf1d112f5efb9caebfbe415859eb02
fnkk <1748501327@qq.com>
2024-04-09 02:05:51 +0800

    feat: first commit

---
+++ layout/index.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import Header from './header/index'
+import Activity from './activity/index'
+
+const Layout = () => {
+    return (
+        <>
+            {/* <Header/> */}
+            <main className='main_box'>
+                <Activity />
+            </main>
+            <div className='footer'>
+                
+            </div>
+        </>
+
+    );
+
+}
+
+export default Layout;
